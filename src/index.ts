172b5e0400dd18d9cdfccbc1dca4e1ff8352020d
Lucas <staroscky60@gmail.com>
2022-06-14 18:24:30 -0300

    feat: creating user entity and connect with database

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
-import "reflect-metadata"
 import express, { Request, Response } from "express"
 import morgan from "morgan"
+import AppDataSource from "./data-source"
+import "reflect-metadata"
+import { User } from "./entities/User"
 
-const app = express()
+const app: express.Application = express()
 
 const bodyParser = express.json()
 
@@ -11,6 +13,23 @@ const PORT = 3000
 app.use(bodyParser)
 app.use(morgan("combined"))
 
-app.get("/ping", (_request: Request, response: Response) => response.send("pong"))
+app.get("/ping", (_request: Request, response: Response) => {
+  response.send("pong")
+})
+
+AppDataSource.initialize()
+  .then(() => {
+    const user = new User()
+    user.email = 'teste@teste.com'
+    user.password = 'password'
+
+    const repository = AppDataSource.getRepository(User)
+    repository.save(user)
+
+    console.log("Data Source has been initialized!")
+  })
+  .catch((err) => {
+    console.error("Error during Data Source initialization", err)
+  })
 
 app.listen(PORT, () => console.log(`listening on port ${PORT}`))
